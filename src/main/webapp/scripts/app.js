1bd9a02f42ef39f76ce792b0130fdb794b19244c
Your Name <you@example.com>
2017-11-20 19:56:56 -0500

    asd888888

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -1,4 +1,6 @@
-angular.module("ngdemoApp", ['ngRoute', 'ngdemoApp.services', 'ngdemoApp.controllers'])
+angular.module('ngdemoApp.controllers', []);
+
+angular.module("ngdemoApp", ['ngRoute','ngdemoApp.controllers' ])
 .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 	  
 	  
@@ -6,17 +8,21 @@ angular.module("ngdemoApp", ['ngRoute', 'ngdemoApp.services', 'ngdemoApp.control
 //	  programa-list de donde viene
 		$routeProvider.
 		//microcurriculos
-		when('/materia1', {templateUrl: 'views/Microcurriculos.html', controller: 'MicrocurriculoListCtrl'}).
-		when('/materia1', {templateUrl: 'views/Microcurriculos.html', controller: 'MicrocurriculoListCtrl'}).
+//		when('/materia1', {templateUrl: 'views/Microcurriculos.html', controller: 'MicrocurriculoListCtrl'}).
+//		when('/materia1', {templateUrl: 'views/Microcurriculos.html', controller: 'MicrocurriculoListCtrl'}).
+//		
+//		when('/programa-creation', {templateUrl: 'views/programa-creation.html', controller: 'ProgramaCreationCtrl'}).
+//		when('/programa-details', {templateUrl: 'views/programa-details.html', controller: 'ProgramaListCtrl'}).
+//		when('/universidad-create', {templateUrl: 'views/universidad-create.html', controller: 'UniversidadCreateCtrl'}).
+//		when('/universidad-list', {templateUrl: 'views/universidad-list.html', controller: 'UniversidadListCtrl'}).
+//		when('/universidad-detail/:idP/:idD/:idC', {templateUrl: 'views/universidad-detail.html', controller: 'UniversidadDetailCtrl'}).
+//		when('/universidad-view/:idP/:idD/:idC', {templateUrl: 'views/universidad-view.html', controller: 'UniversidadDetailCtrl'}).
+//		when('/microcurriculo-view', {templateUrl: 'views/Microcurriculos.html', controller: 'MicrocurriculoListCtrl'}).
+//		
+		when('/main', {templateUrl: 'views/micro-curriculos.html',controller:'MicroCurriculosCtrl'}).
+
 		
-		when('/programa-creation', {templateUrl: 'views/programa-creation.html', controller: 'ProgramaCreationCtrl'}).
-		when('/programa-details', {templateUrl: 'views/programa-details.html', controller: 'ProgramaListCtrl'}).
-		when('/universidad-create', {templateUrl: 'views/universidad-create.html', controller: 'UniversidadCreateCtrl'}).
-		when('/universidad-list', {templateUrl: 'views/universidad-list.html', controller: 'UniversidadListCtrl'}).
-		when('/universidad-detail/:idP/:idD/:idC', {templateUrl: 'views/universidad-detail.html', controller: 'UniversidadDetailCtrl'}).
-		when('/universidad-view/:idP/:idD/:idC', {templateUrl: 'views/universidad-view.html', controller: 'UniversidadDetailCtrl'}).
-		when('/microcurriculo-view', {templateUrl: 'views/Microcurriculos.html', controller: 'MicrocurriculoListCtrl'}).
-		otherwise('/index');
+		otherwise('/main');
 		
 	  //$locationProvider.hashPrefix('');
 	  
@@ -25,3 +31,4 @@ angular.module("ngdemoApp", ['ngRoute', 'ngdemoApp.services', 'ngdemoApp.control
 	  /*$httpProvider.defaults.useXDomain = true;
 	  delete $httpProvider.defaults.headers.common['X-Requested-With'];*/
 }]);
+
---
+++ src/main/webapp/scripts/controllers/microcurrilulos.ctrl.js
@@ -0,0 +1,8 @@
+var module = angular.module('ngdemoApp.controllers');
+
+
+module.controller('MicroCurriculosCtrl', ['$scope', '$location',
+	function ($scope, $location) {
+
+
+}]);
